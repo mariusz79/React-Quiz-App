851c02dcb4e8d291b5fb394783406c7c1a9b41f6
mariusz79 <mpawlowic@gmail.com>
2021-06-13 02:30:22 +0100

    Webpack configured

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h2>Welcome to Quiz App!</h2>
+            <h3>Date: {new Date().toDateString() }</h3>
+        </div>
+    )
+}
+
+export default App
